73267333a32eac2ae30c29b61d08c07a94c5ced9
Jordan Wadjie <jordanwadjie1@gmail.com>
2024-01-10 17:57:53 +0100

    final commit

--- App.jsx
+++ App.jsx
@@ -1,10 +1,9 @@
-
 import imgTailwind from "./assets/Tailwind.png";
 import imgHltml from "./assets/html.png";
 import imgVb from "./assets/vb.png";
 import imgCss from "./assets/css.png";
 import Carousel from "./Components/Carousel";
-import imgJs from "./assets/js.png"
+import imgJs from "./assets/js.png";
 function App() {
   const TableOfimage = [imgJs, imgTailwind, imgHltml, imgVb, imgCss];
 
@@ -16,19 +15,13 @@ function App() {
     <>
       <main>
 
-{/* <div className="relative w-64 h-64">
-  <img src={imgJs} alt="An image" className="absolute inset-0 w-60 h-60 object-cover"/>
-  <div className="absolute inset-0 bg-black opacity-50"></div>
-  <div className="absolute bottom-0 left-0 p-4 text-white">
-    <h2 className="text-2xl font-bold">Title</h2>
-    <p className=" text-lg">Description</p>
-  </div>
-</div> */}
 
         <div className="max-w-lg ">
-          <Carousel>{ContentImg}</Carousel>
+          <Carousel iSautoSlide={true} slideInterval={3000}>
+            {ContentImg}
+          </Carousel>
         </div>
-        
+
       </main>
     </>
   );
--- Components/Carousel.jsx
+++ Components/Carousel.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
-import { ChevronLeft, ChevronRight } from "react-feather";
+import { useEffect, useState } from "react";
 
-const Carousel = ({ children }) => {
-  const [currentImage, setCurrentImage] = useState(0);
+
+const Carousel = ({ children,iSautoSlide,slideInterval }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   const prevSlide = () => {
-    setCurrentImage((prevImage) => {
+    setCurrentImageIndex((prevImage) => {
       if (prevImage === 0) {
         return children.length - 1;
       } else {
@@ -14,7 +14,7 @@ const Carousel = ({ children }) => {
     });
   };
   const nextSlide = () => {
-    setCurrentImage((prevImage) => {
+    setCurrentImageIndex((prevImage) => {
       if (prevImage === children.length - 1) {
         return 0;
       } else {
@@ -24,26 +24,45 @@ const Carousel = ({ children }) => {
   };
 
   const chevronButtonStyle =
-    "p-1 rounded-full bg-blue-600/80 text-white hover:bg-blue-600 shadow ";
+    "p-1 rounded-full bg-blue-600/80 text-white hover:bg-blue-600 shadow h-10 w-10 text-3xl  text-center flex items-center";
+
+    useEffect(() => {
+if(!iSautoSlide){return}
+
+    const autoSlide= setInterval(  nextSlide
+     , slideInterval);
+      return () => {
+       clearInterval(autoSlide)
+      }
+    }, [])
+    
   return (
     <div className="overflow-hidden relative translate-x-0">
       <div
         className={`flex transition-transform ease-out duration-500 `}
-         style={{transform:`translateX(-${currentImage*100}%)`}}
+        style={{ transform: `translateX(-${currentImageIndex * 100}%)` }}
       >
         {children}
       </div>
       <div className="absolute inset-0 flex items-center justify-between">
         <button onClick={prevSlide} className={chevronButtonStyle}>
-          <ChevronLeft className="" size={40} />
+          <h1 className="font-bold ml-[0.26rem] mb-[0.26rem]" >{'<'}</h1>
         </button>
         <button className={chevronButtonStyle}>
-          <ChevronRight onClick={nextSlide} className="" size={40} />
+          <h1 onClick={nextSlide} className=" font-bold ml-[0.26rem] mb-[0.26rem]">{'>'}</h1>
         </button>
       </div>
       <div className="absolute bottom-4 right-0 left-0">
-      <div className="flex items-center gap"></div>
-
+        <div className="flex items-center justify-center gap-2">
+          {children.map((_, imageIndex) => (
+            <div
+              className={`transition-all w-3 h-3 bg-cyan-950 rounded-full ${
+                currentImageIndex === imageIndex ? "p-3" : "bg-opacity-50"
+              }`}
+              key={imageIndex}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
